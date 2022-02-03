b5513fd11777ccaeecbffa891d7919fcc25f6141
tusharjain8177 <67454862+tusharjain8177@users.noreply.github.com>
2022-02-03 08:52:03 +0530

    hi
    hi

--- mychat/settings.py
+++ mychat/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 import django_heroku
-django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,3 +131,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
